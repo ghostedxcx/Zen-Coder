e33cf717ad4de721725d86d768433b30bc50bb9c
Ghxsted <101996880+ghostedxcx@users.noreply.github.com>
2024-05-02 06:09:27 +0100

    Add first Init

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import CodeEditor from './ZenEditor.svelte';
+</script>
+
+<main>
+  <h1>ZenCoder</h1>
+  <CodeEditor />
+</main>
---
+++ src/ZenEditor.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { EditorView, keymap, lineNumbers } from '@codemirror/view';
+    import { EditorState } from '@codemirror/state';
+    import { defaultKeymap } from '@codemirror/commands';
+    import { javascript } from '@codemirror/lang-javascript';
+
+    let editor: EditorView;
+
+    onMount(() => {
+        const state = EditorState.create({
+            doc: 'console.log("Hello, World!");',
+            extensions: [
+                keymap.of(defaultKeymap),
+                lineNumbers(),
+                javascript(),
+            ],
+        });
+
+        editor = new EditorView({
+            state,
+            parent: document.getElementById('editor'),
+        });
+    });
+</script>
+
+<div id="editor"></div>
+
+<style>
+    #editor {
+        height: 400px;
+    }
+</style>
